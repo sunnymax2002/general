fa93e8045898b384b417b501e0f75202ca007fb4
sunnymax2002 <63592658+sunnymax2002@users.noreply.github.com>
2024-12-25 05:59:56 +0530

    Update main.rs

--- source/parallel_prog/rust/hello_rust/src/main.rs
+++ source/parallel_prog/rust/hello_rust/src/main.rs
@@ -2,15 +2,28 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    /* Spawning threads
     thread::spawn(|| {
-        for i in 1..10 {
+        for i in 1..100 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
-    for i in 1..5 {
+    for i in 1..50 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
-    }
+    } */
+
+    /* References: To see if rust compiler introduces barrier before creating mutable reference to prevent HW re-ordering the statements
+     */
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
